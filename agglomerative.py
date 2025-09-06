7a8d1775d1051f2c94d6bde9be073116fcc9a7ca
Dolly Aswin <dolly.aswin@gmail.com>
2025-09-06 22:05:55 +0700

    agglomerative.py implements lib/datasheet

--- agglomerative.py
+++ agglomerative.py
@@ -1,5 +1,3 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -11,9 +9,22 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import silhouette_score
 
+# Datasheet
+from lib.datasheet import DataSheet
 
-# --- data visualiation from clustering result ---
-def v_clustering(n_clusters, df_result, price_map):
+# --- Create Clustering ---
+def clustering(n_clusters, data_scale):
+    # Initialize the Agglomerative Clustering model
+    cluster = AgglomerativeClustering(n_clusters, metric='euclidean', linkage='ward')
+    # Fit the model and get the cluster labels
+    clustering_result = cluster.fit_predict(data_scale)
+
+    return clustering_result
+
+# --- Data visualization from clustering result ---
+def v_clustering(n_clusters, df, clustering_result):
+    df_result = df.copy()
+    df_result['Cluster'] = clustering_result
     plt.figure(figsize=(14, 9))
     sns.scatterplot(
         data=df_result,
@@ -29,11 +40,6 @@ def v_clustering(n_clusters, df_result, price_map):
     plt.xlabel('Customer Rating', fontsize=14)
     plt.ylabel('Price Level (Encoded)', fontsize=14)
 
-    # Use original price labels for y-axis ticks for better interpretation
-    price_ticks = sorted(df_result['Price'].unique())
-    price_labels = [key for key, val in sorted(price_map.items(), key=lambda item: item[1]) if val in price_ticks]
-    #plt.yticks(ticks=price_ticks, labels=price_labels)
-
     plt.legend(title='Cluster')
     plt.grid(True)
     plt.show()
@@ -43,10 +49,10 @@ def v_silhouette(k_range, data_scale):
     silhouette_scores = []
 
     for k in k_range:
-        clustering_result = clustering(k, data_scale)
+        result = clustering(k, data_scale)
 
         # Count silhouette score
-        score = silhouette_score(data_scale, clustering_result)
+        score = silhouette_score(data_scale, result)
         silhouette_scores.append(score)
         print(f"For K={k}, THe Silhouette Score: {score:.4f}")
 
@@ -77,116 +83,41 @@ def v_dendrogram(data_scale, method='ward'):
     plt.ylabel('Euclidean Distance (Ward)', fontsize=12)
     plt.show()
 
-# --- Training data ---
-def training(df_processed, price_map):
-    # Replace comma with dot and convert to numeric
-    df_processed['Price']  = df_processed['Price'].map(price_map)
-
-    # Replace the string 'null' with NumPy's NaN (Not a Number)
-    df_processed.replace('null', np.nan, inplace=True)
-
-    # Fill missing values with the mode (most frequent value) of each column
-    for column in df_processed.columns:
-        if df_processed[column].isnull().any():
-            mode_value = df_processed[column].mode()[0]
-            df_processed[column].fillna(mode_value, inplace=True)
-            print(f"Filled missing values in '{column}' with mode: '{mode_value}'")
-
-    return df_processed
-
-# --- Scaling ---
-def scaling(scaller, data):
-    X_2d = data[['Price', 'Rating']].copy()
-    X_scaled = scaler.fit_transform(X_2d)
-
-    print("\nFeature scaling complete.")
-    return X_scaled
-
-# --- Encoding Price to define the order of the categories ---
-def price_encoding():
-    pricing_map = {'Low': 1, 'low': 1, 'Average': 2, 'average': 2, 'Medium': 3, 'medium': 3, 'High': 4, 'high': 4,
-                   'very-high': 5}
-    return pricing_map
-
-# --- Create Clustering ---
-def clustering(n_clusters, data_scale):
-    # Initialize the Agglomerative Clustering model
-    cluster = AgglomerativeClustering(n_clusters, metric='euclidean', linkage='ward')
-    # Fit the model and get the cluster labels
-    clustering_result = cluster.fit_predict(data_scale)
-
-    return clustering_result
-
-# --- Write New Datasheet With Cluster Label ---
-def create_new_datasheet(cluster):
-    df['Cluster'] = cluster
-
-    # # Save the final dataframe to a new CSV file
-    df.to_csv(output_dataset_file_path, index=False, sep=';')
-
-    print(f"\nSuccessfully saved the new dataset with cluster labels to '{output_dataset_file_path}'")
-    print("\nFirst 5 rows of the new dataset:")
-    print(df.head())
-
-    print(f"\nSuccessfully saved the new dataset to '{output_dataset_file_path}'")
-
-def load_datasheet(dataset_file_path):
-    try:
-        df = pd.read_excel(dataset_file_path)
-        print("Dataset loaded successfully!")
-    except FileNotFoundError:
-        print("Error: 'Attribute DataSet.xlsx' not found. Please make sure the file is in the correct directory.")
-        exit()
-
-    # --- Initial Inspection ---
-    print("\nFirst 5 rows of the dataset:")
-    print(df[['Dress_ID', 'Style', 'Price', 'Rating', 'Size', 'Season', 'NeckLine', 'SleeveLength']].head())
-
-    print("\nDataset Information (dtypes, non-null counts):")
-    df.info()
-    return df
-
 
 # Set plot style for better visuals
 sns.set(style="whitegrid")
 import warnings
 warnings.filterwarnings('ignore')
 
+# The optimal k based on Silhouette
+optimal_k = 10
+
 # Clustering starts at 2 groups, the number of samples (n) = 501
 # Square root of n = sqrt(501) ≈ 22.38
 k_range = range(2, 25)
 
-# The optimal k based on Silhouette
-optimal_k = 10
-
-# New Datasheet with clustering result
-output_dataset_file_path = 'assets/dress-datasheet-agglomerative-clustering.csv'
-
-# Load the datasheet
+# Datasheet file
 dataset_file_path = 'assets/Attribute DataSet.xlsx'
+# New Datasheet with clustering result
+output_dataset_file_path = 'assets/dress-datasheet-k-means-clustering.csv'
 
-df = load_datasheet(dataset_file_path)
-df_copy = df.copy()
-scaler  = StandardScaler()
-
-# Training and scaling the data
-pricing_map  = price_encoding()
-df_processed = training(df_copy, pricing_map)
-data_scaled  = scaling(scaler, df_processed)
+scaler = StandardScaler()
+ds = DataSheet(dataset_file_path, scaler)
+ds.load()
+ds.training()
 
 # Create visualization of dendrogram
-v_dendrogram(data_scaled)
+v_dendrogram(ds.get_data_scaled())
 
 # Create visualization of methods
-v_silhouette(k_range, data_scaled)
+v_silhouette(k_range, ds.get_data_scaled())
 
 # Run clustering with optimal k
-clustering_result = clustering(optimal_k, data_scaled)
+clustering_result = clustering(optimal_k, ds.get_data_scaled())
 
 # Add the cluster labels to our processed (but not scaled) DataFrame
-df_processed['Cluster'] = clustering_result
-# Create visualization of Clustering
-v_clustering(optimal_k, df_processed, pricing_map)
+v_clustering(optimal_k, ds.get_df(), clustering_result)
 
 # Create new datasheet with cluster label
-create_new_datasheet(clustering_result)
+clusters = clustering_result
+ds.save_clusters(clusters, output_dataset_file_path)
